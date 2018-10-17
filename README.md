9a22a5d1e39bd1333764ec489b797ab8000f3971
Vasile Claudiu <claudiu.vasile@gmail.com>
2018-10-17 16:18:05 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-reactmaterial
+<!DOCTYPE html>
+<html>
+<head>
+	<h1>React Material</h1>
+</head>
+<body>
+	<h1>React App Material</h1>
+	<a href="https://vasileclaudiu.github.io/reactmaterial/"><strong>DEMO</strong></a>
+</body>
+</html>
---
+++ precache-manifest.f56bd5eef3dba84bff6232e462bd0d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8236ca8708ffb080751",
+    "url": "/static/js/main.a8236ca8.chunk.js"
+  },
+  {
+    "revision": "79379f516ad11160c681",
+    "url": "/static/js/1.79379f51.chunk.js"
+  },
+  {
+    "revision": "a8236ca8708ffb080751",
+    "url": "/static/css/main.825f2b43.chunk.css"
+  },
+  {
+    "revision": "051fef9db6dee36e24a7892026f51822",
+    "url": "/index.html"
+  }
+];
