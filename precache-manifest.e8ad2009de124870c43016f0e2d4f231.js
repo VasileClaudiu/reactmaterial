8da17a4764c0dfd1413a28235b18a0ff60545c01
Vasile Claudiu <claudiu.vasile@gmail.com>
2018-10-17 16:28:32 +0300

    Second commit

---
+++ precache-manifest.e8ad2009de124870c43016f0e2d4f231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactmaterial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92b0e60f2193fb8c612b",
+    "url": "/reactmaterial/static/js/runtime~main.92b0e60f.js"
+  },
+  {
+    "revision": "fbcbc0432458b1bb8a16",
+    "url": "/reactmaterial/static/js/main.fbcbc043.chunk.js"
+  },
+  {
+    "revision": "79379f516ad11160c681",
+    "url": "/reactmaterial/static/js/1.79379f51.chunk.js"
+  },
+  {
+    "revision": "fbcbc0432458b1bb8a16",
+    "url": "/reactmaterial/static/css/main.825f2b43.chunk.css"
+  },
+  {
+    "revision": "676113412a40d9f0fa734b9cbea3a86d",
+    "url": "/reactmaterial/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f56bd5eef3dba84bff6232e462bd0d56.js"
+  "/reactmaterial/precache-manifest.e8ad2009de124870c43016f0e2d4f231.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reactmaterial/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
