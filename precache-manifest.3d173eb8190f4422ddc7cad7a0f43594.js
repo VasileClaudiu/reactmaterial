3f585877060800588e9fa330b21ec2bd176942ea
Vasile Claudiu <claudiu.vasile@gmail.com>
2018-10-25 14:41:46 +0300

    add login functionality

---
+++ precache-manifest.3d173eb8190f4422ddc7cad7a0f43594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactmaterial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92b0e60f2193fb8c612b",
+    "url": "/reactmaterial/static/js/runtime~main.92b0e60f.js"
+  },
+  {
+    "revision": "0b64153b562dffc18e19",
+    "url": "/reactmaterial/static/js/main.0b64153b.chunk.js"
+  },
+  {
+    "revision": "d958c04df0a09fde56c5",
+    "url": "/reactmaterial/static/js/1.d958c04d.chunk.js"
+  },
+  {
+    "revision": "0b64153b562dffc18e19",
+    "url": "/reactmaterial/static/css/main.0a9fefc5.chunk.css"
+  },
+  {
+    "revision": "9d3c3c554d1e99c6b64c82ad76a4cc88",
+    "url": "/reactmaterial/index.html"
+  }
+];
