c65c8ae7b7e78da965c4e2b8bb1c2d40b18fa2a7
Vasile Claudiu <claudiu.vasile@gmail.com>
2018-11-02 21:32:54 +0200

    bug fix

---
+++ precache-manifest.a8a507ea4ff7556be6b98fe0cc3380fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactmaterial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92b0e60f2193fb8c612b",
+    "url": "/reactmaterial/static/js/runtime~main.92b0e60f.js"
+  },
+  {
+    "revision": "15878d8ccf8c2771ab06",
+    "url": "/reactmaterial/static/js/main.15878d8c.chunk.js"
+  },
+  {
+    "revision": "c3e0c5df178388e74f7c",
+    "url": "/reactmaterial/static/js/1.c3e0c5df.chunk.js"
+  },
+  {
+    "revision": "15878d8ccf8c2771ab06",
+    "url": "/reactmaterial/static/css/main.90fc526e.chunk.css"
+  },
+  {
+    "revision": "8bc3c8c951bc56ed07389a042593c748",
+    "url": "/reactmaterial/index.html"
+  }
+];
